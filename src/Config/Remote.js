9879f00aaea191852fb87c637295ac275fc308d4
EagleVee <nnavu1@gmail.com>
2019-12-10 15:23:15 +0700

    remove

--- src/Config/Remote.js
+++ src/Config/Remote.js
@@ -1,12 +1,7 @@
-// export const API_ENDPOINT = (process.env.ENVIRONMENT === 'proxy-local')
-//   ? 'https://cors-anywhere.herokuapp.com/http://165.22.49.37/api'
-//   : 'http://165.22.49.37/api'
 export const API_ENDPOINT =
   process.env.REACT_APP_ENVIRONMENT === "local"
     ? process.env.REACT_APP_API_ENDPOINT_LOCAL
     : process.env.REACT_APP_API_ENDPOINT;
-export const API_PUSHER_ENDPOINT = process.env.REACT_APP_API_PUSHER;
-console.log(API_ENDPOINT);
 export const REQUEST_TIME_OUT = 30000;
 export const STATUS_OK = 200;
 export const STATUS_BAD_REQUEST = 400;
