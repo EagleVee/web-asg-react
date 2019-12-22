1beef9eed5dd8040f583937229040e3ea4b3f980
EagleVee <nnavu1@gmail.com>
2019-12-22 17:46:16 +0700

    add global

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import createStore from "./Redux";
 import RootContainer from "./Containers/RootContainer";
 // create our store
