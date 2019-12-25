f308e256c916322dd87229890371f999b44911c4
EagleVee <nnavu1@gmail.com>
2019-12-25 09:42:10 +0700

    render admin and student menu

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -32,18 +32,36 @@ class Container extends Component {
   }
 
   renderMenu() {
+    const { role } = this.props.auth.user;
+    if (role === "student") {
+      return this.renderStudentMenu();
+    } else if (role === "admin") {
+      return this.renderAdminMenu();
+    }
     return (
       <Menu
         theme="dark"
         style={{ height: "100vh" }}
         defaultSelectedKeys={["1"]}
-        defaultOpenKeys={["sub-message"]}
+        defaultOpenKeys={["1"]}
+        mode="inline"
+      />
+    );
+  }
+
+  renderStudentMenu() {
+    return (
+      <Menu
+        theme="dark"
+        style={{ height: "100vh" }}
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["1"]}
         mode="inline"
       >
         <Menu.SubMenu
-          key="conversation"
+          key="1"
           onTitleClick={() => {
-            this.props.menuOnClick("conversation");
+            this.props.menuOnClick("1");
           }}
           title={
             <span className={styles.viewCenter}>
@@ -59,9 +77,59 @@ class Container extends Component {
       </Menu>
     );
   }
+
+  renderAdminMenu() {
+    return (
+      <Menu
+        theme="dark"
+        style={{ height: "100vh" }}
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["1"]}
+        mode="inline"
+      >
+        <Menu.SubMenu
+          key="1"
+          onTitleClick={() => {
+            this.props.menuOnClick("1");
+          }}
+          title={
+            <span className={styles.viewCenter}>
+              <Icon type="calendar" style={{ fontSize: 16 }} />
+              <span>Đăng ký thi</span>
+            </span>
+          }
+        >
+          <Menu.Item key="class">
+            <Link to="/class">Danh sách môn học</Link>
+          </Menu.Item>
+          <Menu.Item key="shift">
+            <Link to="/shift">Ca thi</Link>
+          </Menu.Item>
+        </Menu.SubMenu>
+        <Menu.SubMenu
+          key="2"
+          onTitleClick={() => {
+            this.props.menuOnClick("2");
+          }}
+          title={
+            <span className={styles.viewCenter}>
+              <Icon type="user" style={{ fontSize: 16 }} />
+              <span>Tài khoản</span>
+            </span>
+          }
+        >
+          <Menu.Item key="manage">
+            <Link to="/account/manage">Quản lý tài khoản</Link>
+          </Menu.Item>
+        </Menu.SubMenu>
+      </Menu>
+    );
+  }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
 const mapDispatchToProps = dispatch => ({});
 
