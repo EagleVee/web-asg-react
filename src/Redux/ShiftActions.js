0f289b424f467821646e04052ad502375fe95f7f
Tien Tran <tunlunhn1999@gmail.com>
2019-12-28 07:37:51 +0700

    shift detail page added

--- src/Redux/ShiftActions.js
+++ src/Redux/ShiftActions.js
@@ -3,10 +3,14 @@ import { createActions } from "reduxsauce";
 const { Types, Creators } = createActions({
   getListShift: ["params", "onSuccess", "onFailed"],
   getListShiftSuccess: ["response"],
+  getListShiftDetail: ["params", "onSuccess", "onFailed"],
+  getListShiftDetailSuccess: ["response"],
   updateShift: ["id", "data", "onSuccess", "onFailed"],
   updateShiftSuccess: ["response"],
   createShift: ["data", "onSuccess", "onFailed"],
-  uploadShift: ["data", "onSuccess", "onFailed"]
+  uploadShift: ["data", "onSuccess", "onFailed"],
+  uploadListShiftDetail: ["params", "onSuccess", "onFailed"],
+  getListShiftDetailSuccess: ["response"]
 });
 
 export const ShiftTypes = Types;
